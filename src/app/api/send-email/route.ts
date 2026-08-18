c1def94fec97b4c6db35a67dbc5f551337535b05
Jundrel Alonzo <93656917+Provi-dence@users.noreply.github.com>
2026-08-19 01:47:51 +0800

    Hofix Send-Email Route V7.3

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
 export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
 
 export async function POST(req: Request) {
   try {
